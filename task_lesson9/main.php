738c41bb17810cd3d10dd2b9633543e968c7dd06
larissolb <fasolya@gmail.com>
2018-12-13 20:31:03 +0300

    fix

--- task_lesson9/main.php
+++ task_lesson9/main.php
@@ -1,26 +1,104 @@
 <?php
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Operation
+ *
+ * @author larissolb
+ */
+
+
+class Operation {
+    protected $a;
+    protected $b;
+    
+      public function __construct($a, $b) {
         
-include_once 'Operation.php';
-include_once 'Plus.php';
-include_once 'Minus.php';
+        $this->a = $a;
+        $this->b = $b;
+    }
+    
+    public function execute() {
+      return;
+    }
+    
+}
 
-//создание объекта
-$operation = new Operation();
+class Plus extends Operation {
+    
+    function __construct($a, $b) {
+
+        parent::__construct($a, $b);
+    }
+    
+    
+    public function execute(){
+        
+        return $this->a + $this->b;
+    
+    }
+   
+}
+
+class Minus extends Operation {
+    
+    public function __construct($a, $b) {
+
+        parent::__construct($a, $b);
+    }
+    
+    
+    public function execute(){
+      return $this->a - $this->b;
+    
+    
+    }
+    
+}
+
+class Calculator {
+    
+    public static function getInstance($a, $b, $operation){
+         if($operation == "+"){
+             return new Plus($a, $b);
+         }
+         return new Minus($a, $b);
+}
+}
+
+$post = $_POST;
+$a = $post["a"];
+$b = $post["b"];
+$op = $post["operation"];
+$sum = $a+$b;
 
 
+$operation = Calculator::getInstance($a, $b, $op);
+$operation->execute();
+var_dump($operation);
+var_dump($sum);
 
-$plus = new Plus(3, 4);
-var_dump($plus);
-$plus->execute(3, 4);
-var_dump($plus);
+?>
+<!--//форма из $POST значения и передать их-->
 
-$minus = new Minus(1, 5);
-var_dump($minus);
-$minus->execute(1, 5);
-var_dump($minus);
+<!DOCTYPE html>
+<form name="calculate" action="main.php" method="POST">
+    <input type="number" name="a">
+        <input type="text" name="operation">
+    <input type="number" name="b">
 
+    <input type="submit" value="="> <span><?php echo $sum ?>
+    </span>
 
+    
+    
+    
+</form>
 
 
 
