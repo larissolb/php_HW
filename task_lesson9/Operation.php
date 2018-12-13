8851307ee03f97792dfd2307f774a957cea6417c
larissolb <fasolya@gmail.com>
2018-12-13 20:27:26 +0300

    Revert "add form"
    This reverts commit a26fd3d33a68ec228df20be9b683d49ba8ca66da.

---
+++ task_lesson9/Operation.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Operation
+ *
+ * @author larissolb
+ */
+
+
+class Operation {
+    public $a;
+    public $b;
+    
+      public function __construct($a, $b) {
+        
+        $this->a = $a;
+        $this->b = $b;
+    }
+    
+    public function execute() {
+      
+    }
+    
+   
+}
+
+
